d8308635683ff60a1aff2d717f6e1627e67ef4e7
Alexander Lyons <alyons@hylluspartners.com>
2013-05-10 15:58:17 -0400

    Working on adding enough data to create test to run... using NUnit or something like that...

--- WorkerLibrary/WorkerLibraryTestApp/SudokuWorker.cs
+++ WorkerLibrary/WorkerLibraryTestApp/SudokuWorker.cs
@@ -16,18 +16,32 @@ namespace WorkerLibraryTestApp
         #endregion
 
         #region Properties
+        public bool PuzzleSoved
+        {
+            get { return SudokuSolved(solvedPuzzle); }
+        }
         #endregion
 
         #region Constructors
         public SudokuWorker(string puzzleBase)
             : base()
         {
-            Contract.Requires<ArgumentNullException>(String.IsNullOrWhiteSpace(puzzleBase), "puzzleBase cannot be null, empty, or white space.");
+            Contract.Requires<ArgumentNullException>(!String.IsNullOrWhiteSpace(puzzleBase), "puzzleBase cannot be null, empty, or white space.");
             Contract.Requires<ArgumentException>(IsDigitsOnly(puzzleBase), "puzzleBase must only contain number.");
             Contract.Requires<ArgumentException>(puzzleBase.Count() == 81, "puzzleBase must be 81 characters long.");
 
+            basePuzzle = new int[9, 9];
+            workingPuzzle = new int[9, 9];
+            solvedPuzzle = new int[9, 9];
+
             for (int r = 0; r < 9; r++)
             {
+                for (int c = 0; c < 9; c++)
+                {
+                    basePuzzle[r, c] = puzzleBase[r + c * 9];
+                    workingPuzzle[r, c] = puzzleBase[r + c * 9];
+                    solvedPuzzle[r, c] = puzzleBase[r + c * 9];
+                }
             }
         }
         #endregion
@@ -37,7 +51,6 @@ namespace WorkerLibraryTestApp
         {
             return String.Format("SudokuWorker[{0}]", ID);
         }
-
         private static bool IsDigitsOnly(string str)
         {
             foreach (char c in str)
@@ -47,6 +60,77 @@ namespace WorkerLibraryTestApp
             }
 
             return true;
+        }
+        private static bool SudokuSolved(int[,] sudoku)
+        {
+            Contract.Requires<ArgumentNullException>(sudoku != null, "sudoku cannot be null.");
+
+            for (int r = 0; r < 9; r++)
+                for (int c = 0; c < 9; c++)
+                    if (sudoku[r, c] == 0) return false;
+            
+            return true;
+        }
+        void Solve(int row, int col)
+        {
+            if (row > 8)
+            {
+                solvedPuzzle = workingPuzzle;
+                return;
+            }
+
+            if (workingPuzzle[row, col] != 0)
+            {
+                next(row, col);
+            }
+            else
+            {
+                for (int num = 1; num < 10; num++)
+                {
+                    if (CheckRow(row, num) && CheckColumn(col, num) && CheckSquare(row, col, num))
+                    {
+                        workingPuzzle[row, col] = num;
+                        next(row, col);
+                    }
+                }
+            }
+        }
+        void next(int row, int col)
+        {
+        }
+        bool CheckRow(int row, int num)
+        {
+            for (int c = 0; c < 9; c++)
+                if (workingPuzzle[row, c] == num)
+                    return false;
+
+            return true;
+        }
+        bool CheckColumn(int col, int num)
+        {
+            for (int r = 0; r < 9; r++)
+                if (workingPuzzle[r, col] == num)
+                    return false;
+
+            return true;
+        }
+        bool CheckSquare(int row, int col, int num)
+        {
+            row = (row / 3) * 3;
+            col = (col / 3) * 3;
+
+            for (int r = 0; r < 3; r++)
+                for (int c = 0; c < 3; c++)
+                    if (workingPuzzle[row + r, col + c] == num)
+                        return false;
+
+            return true;
+        }
+        void WriteToConsole()
+        {
+            for (int r = 0; r < 9; r++)
+                for (int c = 0; c < 9; c++)
+                    Console.Write(workingPuzzle[r, c] + " ");
         }
         #endregion
     }
---
+++ WorkerLibrary/WorkerLibraryTestApp/SudokuWorkerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace WorkerLibraryTestApp
+{
+    [TestFixture]
+    public class SudokuWorkerTest
+    {
+        [Test]
+        public void CreateSudoku()
+        {
+            SudokuWorker sudokuWorker = new SudokuWorker("200000060000075030048090100000300000300010009000008000001020570080730000090000004");
+
+            Assert.AreEqual(false, sudokuWorker.PuzzleSoved);
+        }
+    }
+}
